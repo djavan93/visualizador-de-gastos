1c730d31ac32baac3f546017bba3cf2a2bd50a24
Djavan Costa <djavancosta93@gmail.com>
2026-05-31 11:25:59 -0300

    feat(backend): scaffold spring boot via initializr

---
+++ backend/src/main/java/com/djavan/visu_gastos/VisuGastosApplication.java
@@ -0,0 +1,13 @@
+package com.djavan.visu_gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisuGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisuGastosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/djavan/visu_gastos/VisuGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.djavan.visu_gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisuGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
